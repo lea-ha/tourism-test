1f830086de5fca8e188e0edd2444cfab916f4ed1
laraayass <115283756+laraayass@users.noreply.github.com>
2023-11-16 17:17:01 +0200

    Add files via upload

---
+++ Touristic website/validate-email.php
@@ -0,0 +1,16 @@
+<?php
+#custom validation for the email by sending a query
+
+$mysqli = require __DIR__ . "/dbconnect.php";
+
+$sql = sprintf("SELECT * FROM users WHERE email = '%s'", $mysqli->real_escape_string($_GET["email"]));
+
+$result = $mysqli->query($sql);
+
+$is_available = $result->num_rows === 0;
+
+header("Content-Type: application/json");
+
+echo json_encode(["available" => $is_available]);
+
+//syntax to check : http://localhost/PROJECTS/food-test/validate-email.php?email=themail
